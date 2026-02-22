70fe5d0ff044c72d7609ca66f7b980b211e85941
Ryan Rowland <ryan.rowland@gmail.com>
2026-02-22 05:42:09 +0000

    feat(core): add constants

---
+++ src/Mods/RKRModdingExtensions_ad438e6e-67b9-4742-9a27-923a3387d923/Story/RawFiles/Lua/Core/Constants.lua
@@ -0,0 +1,12 @@
+local ANSI_COLOURS = {
+    RESET   = "\27[0m",
+    RED     = "\27[31m",
+    GREEN   = "\27[32m",
+    YELLOW  = "\27[33m",
+    BLUE    = "\27[34m",
+    MAGENTA = "\27[35m",
+    CYAN    = "\27[36m",
+    GREY    = "\27[38;5;245m",
+}
+
+Rkr.Constants.ANSI_COLOURS = ANSI_COLOURS
